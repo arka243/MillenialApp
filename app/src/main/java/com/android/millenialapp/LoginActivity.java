62b83551d2e0eb3ed8232f9a0fff8ff920880670
pritic <changlani.priti@gmail.com>
2015-05-18 16:32:35 -0400

    Added Log tag for easy debugging in LogCat

--- app/src/main/java/com/android/millenialapp/LoginActivity.java
+++ app/src/main/java/com/android/millenialapp/LoginActivity.java
@@ -11,6 +11,7 @@ import android.content.Loader;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.AsyncTask;
+import android.util.Log;
 
 import android.os.Build;
 import android.os.Bundle;
@@ -39,6 +40,9 @@ public class LoginActivity extends Activity implements LoaderCallbacks<Cursor> {
      * A dummy authentication store containing known user names and passwords.
      * TODO: remove after connecting to a real authentication system.
      */
+
+    private static final String Tag = "MillenialApp";
+
     private static final String[] DUMMY_CREDENTIALS = new String[]{
             "foo@example.com:hello", "bar@example.com:world"
     };
@@ -57,6 +61,8 @@ public class LoginActivity extends Activity implements LoaderCallbacks<Cursor> {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Log.i(Tag, "From the Millenial App");
 
         // Set up the login form.
         mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
