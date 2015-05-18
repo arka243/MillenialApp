aa8d18b222a71dd4e0a6a884f635d7dfb73bfe10
pritic <changlani.priti@gmail.com>
2015-05-18 18:42:43 -0400

    Added the drawer with the images, though it still displays as a list and not drawer

---
+++ app/src/main/java/com/android/millenialapp/DrawerActivity.java
@@ -0,0 +1,57 @@
+package com.android.millenialapp;
+
+import android.app.Activity;
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+
+public class DrawerActivity extends Activity {
+
+    ListView list;
+    String[] itemname = {
+            "Assignments",
+            "Messages",
+            "Forum",
+            "Feed",
+            "Results",
+            "Rewards"
+    };
+
+    Integer[] imgid = {
+            R.mipmap.ic_menu_assignments_24dp,
+            R.mipmap.ic_menu_assignments_24dp,
+            R.mipmap.ic_menu_assignments_24dp,
+            R.mipmap.ic_menu_assignments_24dp,
+            R.mipmap.ic_menu_assignments_24dp,
+            R.mipmap.ic_menu_assignments_24dp,
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drawer);
+
+
+        CustomListAdapter adapter = new CustomListAdapter(this, itemname, imgid);
+        list = (ListView) findViewById(R.id.list);
+        list.setAdapter(adapter);
+
+        list.setOnItemClickListener(new OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                // TODO Auto-generated method stub
+                String Slecteditem = itemname[+position];
+                Toast.makeText(getApplicationContext(), Slecteditem, Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+}
