a5fa0de28d881df2dc83abbc4ee4f7a0a68882fe
arka243 <msarka.praharaj@gmail.com>
2015-05-20 17:58:44 -0400

    Included EditProfile

--- app/src/main/java/com/android/millenialapp/ProfileActivity.java
+++ app/src/main/java/com/android/millenialapp/ProfileActivity.java
@@ -1,16 +1,29 @@
 package com.android.millenialapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
+
+import java.util.List;
 
 public class ProfileActivity extends Activity {
 
     private TextView fullName;
     private TextView userTitle;
     private Button editProfile;
+    private Button logOut;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,11 +34,34 @@ public class ProfileActivity extends Activity {
         fullName = (TextView) findViewById(R.id.fullname);
         userTitle = (TextView) findViewById(R.id.title);
         editProfile = (Button) findViewById(R.id.editprofile);
+        editProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(ProfileActivity.this,EditProfileActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
+
+        logOut = (Button) findViewById(R.id.logout);
+        logOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ParseUser.logOut();
+                Intent intent = new Intent(ProfileActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
 
         ParseUser current = ParseUser.getCurrentUser();
-
-        String user = current.getUsername().toString();
-        fullName.setText(user);
-
+        String firstname = current.get("FirstName").toString();
+        String lastname = current.get("LastName").toString();
+        if(firstname.equals("") && lastname.equals("")) {
+            fullName.setText("Anonymus User");
+        }
+        else {
+            fullName.setText(firstname + " " + lastname);
+        }
     }
 }
