5b23f67c63d1f55175a899e3b62cb71fb867ec02
arka243 <msarka.praharaj@gmail.com>
2015-06-01 02:04:44 -0400

    normal and search drawers

--- app/src/main/java/com/android/millenialapp/AssignmentsActivity.java
+++ app/src/main/java/com/android/millenialapp/AssignmentsActivity.java
@@ -1,6 +1,7 @@
 package com.android.millenialapp;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 /**
  * Created by Arkaprava on 5/31/2015.
@@ -11,5 +12,12 @@ public class AssignmentsActivity extends DrawerActivity {
     public void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState, R.layout.activity_drawer);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.drawer, menu);
+        return true;
     }
 }
