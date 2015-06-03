72b2da8b47c9d6cd5015252024bdf9219577fdc8
arka243 <msarka.praharaj@gmail.com>
2015-06-03 13:49:55 -0400

    formatting of Reward and Rewardhistory (populating left only)

--- app/src/main/java/com/android/millenialapp/RewardsActivity.java
+++ app/src/main/java/com/android/millenialapp/RewardsActivity.java
@@ -1,15 +1,30 @@
 package com.android.millenialapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by Arkaprava on 5/31/2015.
  */
 public class RewardsActivity extends DrawerActivity {
 
+    private Button showHistory;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
 
-        super.onCreate(savedInstanceState, R.layout.activity_drawer);
+        super.onCreate(savedInstanceState, R.layout.activity_rewards);
+
+        showHistory = (Button) findViewById(R.id.showhistory);
+        showHistory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RewardsActivity.this, RewardsHistoryActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/android/millenialapp/RewardsHistoryActivity.java
+++ app/src/main/java/com/android/millenialapp/RewardsHistoryActivity.java
@@ -1,9 +1,38 @@
 package com.android.millenialapp;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageButton;
 
 /**
  * Created by Arkaprava on 5/31/2015.
  */
-public class RewardsHistoryActivity extends Activity {
+public class RewardsHistoryActivity extends ActionBarActivity {
+
+    private Toolbar toolbar;
+    private ImageButton backButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reward_history);
+
+        toolbar = (Toolbar) findViewById(R.id.back_tool_bar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        backButton = (ImageButton) findViewById(R.id.backButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RewardsHistoryActivity.this, RewardsActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
 }
