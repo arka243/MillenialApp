87ef7f0546c990234161defaeeba4ee7d6fa2266
arka243 <msarka.praharaj@gmail.com>
2015-06-04 01:21:23 -0400

    completed rewards

--- app/src/main/java/com/android/millenialapp/RewardsActivity.java
+++ app/src/main/java/com/android/millenialapp/RewardsActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 /**
  * Created by Arkaprava on 5/31/2015.
@@ -11,11 +13,25 @@ import android.widget.Button;
 public class RewardsActivity extends DrawerActivity {
 
     private Button showHistory;
+    private EditText companyTitle;
+    private EditText rewardAmount;
+    private TextView rewardTitle;
+    private TextView rewardSubtitle;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState, R.layout.activity_rewards);
+
+        companyTitle = (EditText) findViewById(R.id.companyname);
+        rewardAmount = (EditText) findViewById(R.id.reward);
+        rewardTitle = (TextView) findViewById(R.id.reward_title);
+        rewardSubtitle = (TextView) findViewById(R.id.reward_subtitle);
+
+        companyTitle.setText("Amazon");
+        rewardAmount.setText("$10");
+        rewardTitle.setText("Title Goes Here");
+        rewardSubtitle.setText("Subtitle here");
 
         showHistory = (Button) findViewById(R.id.showhistory);
         showHistory.setOnClickListener(new View.OnClickListener() {
