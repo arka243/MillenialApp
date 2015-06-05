f21b4823d52b8e80dfa770ae33778566c6da01c5
pritic <changlani.priti@gmail.com>
2015-06-05 15:03:43 -0500

    Added the code in AssignmentsActivity for swipe gesture; needs to be tested on real device

--- app/src/main/java/com/android/millenialapp/AssignmentsActivity.java
+++ app/src/main/java/com/android/millenialapp/AssignmentsActivity.java
@@ -1,7 +1,11 @@
 package com.android.millenialapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.GestureDetector;
 import android.view.Menu;
+import android.view.MotionEvent;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
@@ -19,6 +23,8 @@ public class AssignmentsActivity extends DrawerActivity {
     private TextView msgfirstLine3;
     private TextView msgSecondLine3;
     private TextView msgThirdLine3;
+
+    private GestureDetector gestureDetector;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -45,6 +51,8 @@ public class AssignmentsActivity extends DrawerActivity {
         msgSecondLine3.setText("Quiz");
         msgThirdLine3.setText("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut allquip ex ea commodo consequat.");
 
+        gestureDetector = new GestureDetector(
+                new SwipeGestureDetector());
     }
 
     @Override
@@ -52,5 +60,61 @@ public class AssignmentsActivity extends DrawerActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.drawer, menu);
         return true;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (gestureDetector.onTouchEvent(event)) {
+            return true;
+        }
+        return super.onTouchEvent(event);
+    }
+
+    private void onLeftSwipe() {
+        // Do something
+        Intent i = new Intent(this, MessagesActivity.class);
+        startActivity(i);
+    }
+
+    private void onRightSwipe() {
+        // Do something
+        Intent i = new Intent(this, MessagesActivity.class);
+        startActivity(i);
+    }
+
+    // Private class for gestures
+    private class SwipeGestureDetector
+            extends GestureDetector.SimpleOnGestureListener {
+        // Swipe properties, you can change it to make the swipe
+        // longer or shorter and speed
+        private static final int SWIPE_MIN_DISTANCE = 120;
+        private static final int SWIPE_MAX_OFF_PATH = 200;
+        private static final int SWIPE_THRESHOLD_VELOCITY = 200;
+
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2,
+                               float velocityX, float velocityY) {
+            try {
+                float diffAbs = Math.abs(e1.getY() - e2.getY());
+                float diff = e1.getX() - e2.getX();
+
+                if (diffAbs > SWIPE_MAX_OFF_PATH)
+                    return false;
+
+                // Left swipe
+                if (diff > SWIPE_MIN_DISTANCE
+                        && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+                    AssignmentsActivity.this.onLeftSwipe();
+
+                    // Right swipe
+                } else if (-diff > SWIPE_MIN_DISTANCE
+                        && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+                    AssignmentsActivity.this.onRightSwipe();
+                }
+            } catch (Exception e) {
+                Log.e("YourActivity", "Error on gestures");
+            }
+            return false;
+        }
     }
 }
